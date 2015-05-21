2e3b068f7a85af9b238c1c8bcc1284776d78dabe
dario-c <dariocavanillas@gmail.com>
2015-05-21 18:20:47 +0200

    Created seeds for database

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,6 +14,55 @@ class DatabaseSeeder extends Seeder {
 	{
 		Model::unguard();
 
+		Neutrino\Role::create(array( 'name' => 'admin' ));
+		Neutrino\Role::create(array( 'name' => 'author'  ));
+		Neutrino\Role::create(array( 'name' => 'user'  ));
+
+		Neutrino\User::create(array(
+			'name' => 'peter',
+			'email' => 'peter@qwerty.com',
+			'password' => 'password',
+			'role_id' => '1'
+		));
+
+		Neutrino\User::create(array(
+			'name' => 'jane',
+			'email' => 'jane@qwerty.com',
+			'password' => 'password',
+			'role_id' => '2'
+		));
+
+		Neutrino\TextCategory::create(array( 'title' => 'menu' ));
+		Neutrino\TextCategory::create(array( 'title' => 'button' ));
+
+
+		Neutrino\TextKey::create(array( 
+			'title' => 'send_button', 
+			'text_category_id' => '2'
+		));
+
+		Neutrino\TextKey::create(array( 
+			'title' => 'edit_button', 
+			'text_category_id' => '2'
+		));
+
+		Neutrino\TextLanguage::create(array( 
+			'code' => 'eng', 
+			'title' => 'english', 
+		));
+
+		Neutrino\TextValue::create(array( 
+			'value' => 'kng', 
+			'text_key_id' => '1',
+			'text_language_id' => '1'
+		));
+
+		Neutrino\TextValue::create(array( 
+			'value' => 'kng', 
+			'text_key_id' => '2',
+			'text_language_id' => '1'
+		));
+
 		// $this->call('UserTableSeeder');
 	}
 
