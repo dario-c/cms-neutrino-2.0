fe947bc09bd3ab6ab9bbda4abb4652fc327188da
Pascal van Gemert <pascal.vangemert@caviarcontent.com>
2015-05-26 13:34:30 +0200

    Put the cms assets in a folder assets

--- resources/views/cms/base.blade.php
+++ resources/views/cms/base.blade.php
@@ -7,8 +7,8 @@
 		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         
-        <link type="text/css" rel="stylesheet" href="{{ asset('cms/css/libraries.css') }}" />
-        <link type="text/css" rel="stylesheet" href="{{ asset('cms/css/app.css') }}" />
+        <link type="text/css" rel="stylesheet" href="{{ asset('assets/cms/css/libraries.css') }}" />
+        <link type="text/css" rel="stylesheet" href="{{ asset('assets/cms/css/app.css') }}" />
         
         <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -53,7 +53,7 @@
             </div>
         </div>
         
-        <script type="text/javascript" src="{{ asset('cms/js/libraries.js') }}"></script>
+        <script type="text/javascript" src="{{ asset('assets/cms/js/libraries.js') }}"></script>
         <script type="text/javascript">
             
             $("[data-toggle=popover]").popover({
