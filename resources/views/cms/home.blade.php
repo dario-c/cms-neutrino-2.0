05e064ee43a3c443b2796e59e1988b5bad7a5ee6
Pascal van Gemert <pascal.vangemert@caviarcontent.com>
2015-06-01 15:00:14 +0200

    Fixed handling of the auto text keys

--- resources/views/cms/home.blade.php
+++ resources/views/cms/home.blade.php
@@ -5,7 +5,8 @@
 	<h1>Home</h1>
 
 	<div class="panel-body">
-		You are logged in! @textkey('TEXT_KEY_11', 'It works now, Dario I\'m so / happy!', 'category')
+		You are logged in! @textkey('TEXT_KEY_ONE', 'First Text Key', 'category')
+		@textkey('TEXT_KEY_TWO', 'Second Text Key', 'category')
 	</div>
 	
 @endsection
