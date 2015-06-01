82a5ce50f19770ff929573d8cae15902c5ec0759
Pascal van Gemert <pascal.vangemert@caviarcontent.com>
2015-06-01 15:00:39 +0200

    Removed text key frontend handling from CMS home template

--- resources/views/cms/home.blade.php
+++ resources/views/cms/home.blade.php
@@ -5,8 +5,7 @@
 	<h1>Home</h1>
 
 	<div class="panel-body">
-		You are logged in! @textkey('TEXT_KEY_ONE', 'First Text Key', 'category')
-		@textkey('TEXT_KEY_TWO', 'Second Text Key', 'category')
+		You are logged in!
 	</div>
 	
 @endsection
