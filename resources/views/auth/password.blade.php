d511b4d79bd5665bc00f06341417170f42b04e42
dario-c <dariocavanillas@gmail.com>
2015-06-01 16:06:36 +0200

    Resized Auth views sizes and added a class to use as selector

--- resources/views/auth/password.blade.php
+++ resources/views/auth/password.blade.php
@@ -1,9 +1,9 @@
 @extends('cms.base')
 
 @section('content')
-<div class="container-fluid">
+<div class="container-fluid auth">
 	<div class="row">
-		<div class="col-md-8 col-md-offset-2">
+		<div class="col-md-4 col-md-offset-4">
 			<div class="panel panel-default">
 				<div class="panel-heading">Reset Password</div>
 				<div class="panel-body">
