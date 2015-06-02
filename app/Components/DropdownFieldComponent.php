d82f529d34fdfd1bc252ecabed6819941cfe353c
dario-c <dariocavanillas@gmail.com>
2015-06-02 18:06:11 +0200

    Created DropdownFielComponent

---
+++ app/Components/DropdownFieldComponent.php
@@ -0,0 +1,17 @@
+<?php namespace Neutrino\Components;
+	
+class DropdownFieldComponent extends AbstractComponent {
+	
+	/**
+	 * Overwrite getRulesFromParameters function.
+	 * 
+	 * @param array $parameters (default: array())
+	 * @return array
+	 */
+	protected function getRulesFromParameters($field, array $parameters = array())
+	{
+		return array(
+			(isset($parameters['required']) && $parameters['required'] == false) ? null : 'required',
+		);
+	}
+}
