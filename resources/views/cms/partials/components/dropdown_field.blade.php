9f0289746581ddf42e2bdc22d6b0acda8a8c0f5d
dario-c <dariocavanillas@gmail.com>
2015-06-03 11:22:57 +0200

    Used Hardcoded options in select template and validate not empty

--- resources/views/cms/partials/components/dropdown_field.blade.php
+++ resources/views/cms/partials/components/dropdown_field.blade.php
@@ -1,4 +1,9 @@
 <div class="form-group">
 	{!! Form::label($postTypeField->id, $postTypeField->title.(($postTypeField->parameter('required') == true) ? ' *' : '' )) !!}
-    {!! Form::select('role_id', [1,2,3], null,['class' => 'form-control']) !!}
+    {!! Form::select($postTypeField->id, $postTypeField->parameter('source'), null,[
+    	'class' 					=> 'form-control',
+		'required'					=> ($postTypeField->parameter('required') == true) ? 'required' : null, 
+		'data-fv-notempty'			=> ($postTypeField->parameter('required') == true) ? 'true' : null,
+	    'data-fv-notempty-message' 	=> ($postTypeField->parameter('required') == true) ? 'Please select an option' : null,
+    ]) !!}
 </div>
