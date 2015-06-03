d3501bb428e1c930767f9427f61b2278a0145f31
dario-c <dariocavanillas@gmail.com>
2015-06-03 16:46:17 +0200

    Validate dropdown's option existance in table if needed

--- app/Components/DropdownFieldComponent.php
+++ app/Components/DropdownFieldComponent.php
@@ -10,8 +10,34 @@ class DropdownFieldComponent extends AbstractComponent {
 	 */
 	protected function getRulesFromParameters($field, array $parameters = array())
 	{
+		$array = (is_array($parameters['source'])) ? self::rulesCustomOptions($parameters) : self::rulesExistingOptions($parameters);
+		
+		return $array;
+	}
+
+	/**
+	 * Return array with rules for custom options
+	 * 
+	 * @param array $parameters (default: array())
+	 * @return array
+	 */
+	private function rulesCustomOptions(array $parameters)
+	{
 		return array(
-			(isset($parameters['required']) && $parameters['required'] == false) ? null : 'required'
-		);
+				(isset($parameters['required']) && $parameters['required'] == false) ? null : 'required'
+			);
+	}
+
+	/**
+	 * Return array with rules for options in existing table 
+	 * 
+	 * @param array $parameters (default: array())
+	 * @return array
+	 */
+	private function rulesExistingOptions(array $parameters)
+	{
+		return array(
+				(isset($parameters['required']) && $parameters['required'] == false) ? null : 'required', 'exists:'.$parameters['table']
+			);
 	}
 }
