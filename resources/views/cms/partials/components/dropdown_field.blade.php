e33a093a726bc51e0f377d9bdd3f617916334b66
dario-c <dariocavanillas@gmail.com>
2015-06-03 16:03:40 +0200

    Fill Dropdowns when editing Post with previous data

--- resources/views/cms/partials/components/dropdown_field.blade.php
+++ resources/views/cms/partials/components/dropdown_field.blade.php
@@ -1,6 +1,6 @@
 <div class="form-group">
 	{!! Form::label($postTypeField->id, $postTypeField->title.(($postTypeField->parameter('required') == true) ? ' *' : '' )) !!}
-    {!! Form::select($postTypeField->id, $postTypeField->source(), null,[
+    {!! Form::select($postTypeField->id, $postTypeField->source(), (isset($post)) ? $post->getMeta($postTypeField->id) : null,[
     	'class' 					=> 'form-control',
 		'required'					=> ($postTypeField->parameter('required') == true) ? 'required' : null, 
 		'data-fv-notempty'			=> ($postTypeField->parameter('required') == true) ? 'true' : null,
