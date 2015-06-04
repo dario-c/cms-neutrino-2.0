56ce6422636a5fb1949904c17166ad2a62cf39a1
dario-c <dariocavanillas@gmail.com>
2015-06-04 17:37:26 +0200

    Changed RadioButtonComponent to extend CheckBoxComponent directly

--- app/Components/RadioButtonComponent.php
+++ app/Components/RadioButtonComponent.php
@@ -1,18 +1,5 @@
 <?php namespace Neutrino\Components;
 	
-class CheckboxComponent extends AbstractComponent {
-
-	/**
-	 * Process (modify) the fields before they are stored.
-	 * 
-	 * @param string $fieldName
-	 * @param array $requestParameters
-	 * @return array
-	 */
-	public function process($fieldName, array $requestParameters)
-	{		
-		if(isset($requestParameters[$fieldName])) $requestParameters[$fieldName] = implode(',', $requestParameters[$fieldName]);
-
-		return $requestParameters;
-	}
+class RadioButtonComponent extends CheckBoxComponent {
+	//
 }
