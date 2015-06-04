9c1e6bddcfe5a49af3569ef9cc531b244a69ae1c
dario-c <dariocavanillas@gmail.com>
2015-06-04 11:34:18 +0200

    prefill checkboxes on editing

--- resources/views/cms/partials/components/checkbox.blade.php
+++ resources/views/cms/partials/components/checkbox.blade.php
@@ -1,8 +1,12 @@
 <div class="form-group">
 	<br>
+
 	@foreach($postTypeField->source() as $index => $value)
+
 		{!! Form::label($postTypeField->id.'[]',' ') !!}
-		{!! Form::checkbox($postTypeField->id.'[]', $index, $value,[]) !!}
+		{!! Form::checkbox($postTypeField->id.'[]', $index, 
+			(strpos($post->getMeta($postTypeField->id), $value) !== false) ? true : false,
+			[]) !!}
 		{{ $index }}
 	@endforeach
 </div>
