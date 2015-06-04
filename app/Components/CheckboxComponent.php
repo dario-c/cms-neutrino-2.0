c1c70e7147a2ab0077e15c6e09e54192112240a0
dario-c <dariocavanillas@gmail.com>
2015-06-04 13:14:44 +0200

    Removed unnecessary overwriting of getRulesFromParameters

--- app/Components/CheckboxComponent.php
+++ app/Components/CheckboxComponent.php
@@ -15,17 +15,4 @@ class CheckboxComponent extends AbstractComponent {
 
 		return $requestParameters;
 	}
-	
-	/**
-	 * Overwrite getRulesFromParameters function.
-	 * 
-	 * @param array $parameters (default: array())
-	 * @return array
-	 */
-	protected function getRulesFromParameters($field, array $parameters = array())
-	{
-		return array(
-			(isset($parameters['required']) && $parameters['required'] == false) ? null : 'required'
-		);
-	}
 }
