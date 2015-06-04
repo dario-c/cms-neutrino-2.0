69eb3f6272aae6f34b50a9cfb6b60060d9dc8ab7
dario-c <dariocavanillas@gmail.com>
2015-06-04 12:00:08 +0200

    Added frontend validation for required checkbox

--- resources/views/cms/partials/components/checkbox.blade.php
+++ resources/views/cms/partials/components/checkbox.blade.php
@@ -5,7 +5,12 @@
 
 		{!! Form::label($postTypeField->id.'[]',' ') !!}
 		{!! Form::checkbox($postTypeField->id.'[]', $index, 
-			(strpos($post->getMeta($postTypeField->id), $value) !== false) ? true : false, []) !!}
+			(strpos($post->getMeta($postTypeField->id), $value) !== false) ? true : false,
+			[
+				'required'					=> ($postTypeField->parameter('required') == true) ? 'required' : null, 
+				'data-fv-notempty'			=> ($postTypeField->parameter('required') == true) ? 'true' : null,
+			    'data-fv-notempty-message' 	=> ($postTypeField->parameter('required') == true) ? 'Please select an option' : null,
+		]) !!}
 		{{ $index }}
 	@endforeach
 </div>
