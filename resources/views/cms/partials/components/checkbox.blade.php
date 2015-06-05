7fee18578a706f9367bd669191b3bd0293758216
dario-c <dariocavanillas@gmail.com>
2015-06-05 16:03:46 +0200

    Added stying to checkbox template

--- resources/views/cms/partials/components/checkbox.blade.php
+++ resources/views/cms/partials/components/checkbox.blade.php
@@ -1,19 +1,20 @@
 <div class="form-group">
-	<br>
+	<span class="form-label">{{ $postTypeField->title.(($postTypeField->parameter('required') == true) ? ' *' : '')}}</span>
 
-	@foreach($postTypeField->source() as $index => $value)
-		<?php 
-			$fill = false;
-			if(isset($post)) $fill = (strpos($post->getMeta($postTypeField->id), $value) !== false) ? true : false;
-		?>
+	<div class="selection-options">
+		@foreach($postTypeField->source() as $index => $value)
+			<?php 
+				$fill = false;
+				if(isset($post)) $fill = (strpos($post->getMeta($postTypeField->id), $value) !== false) ? true : false;
+			?>
 
-		{!! Form::label($postTypeField->id.'[]',' ') !!}
-		{!! Form::checkbox($postTypeField->id.'[]', $index, $fill,
-			[
-				'required'					=> ($postTypeField->parameter('required') == true) ? 'required' : null, 
-				'data-fv-notempty'			=> ($postTypeField->parameter('required') == true) ? 'true' : null,
-			    'data-fv-notempty-message' 	=> ($postTypeField->parameter('required') == true) ? 'Please select an option' : null,
-		]) !!}
-		{{ $index }}
-	@endforeach
+			{!! Form::label($postTypeField->id.'[]', $index, ['class' => 'selection-label']) !!}
+			{!! Form::checkbox($postTypeField->id.'[]', $index, $fill,
+				[
+					'required'					=> ($postTypeField->parameter('required') == true) ? 'required' : null, 
+					'data-fv-notempty'			=> ($postTypeField->parameter('required') == true) ? 'true' : null,
+				    'data-fv-notempty-message' 	=> ($postTypeField->parameter('required') == true) ? 'Please select an option' : null,
+			]) !!}
+		@endforeach
+	</div>
 </div>
