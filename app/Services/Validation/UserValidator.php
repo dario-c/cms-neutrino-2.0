a763a5ec1da3f954beb8a54677fde525b82a2528
dario-c <dariocavanillas@gmail.com>
2015-06-08 17:18:55 +0200

    Created setRules function which adds different validation rules depending on users existing or not

--- app/Services/Validation/UserValidator.php
+++ app/Services/Validation/UserValidator.php
@@ -8,8 +8,30 @@ class UserValidator extends Validator {
 	 * @var array Validation rules for the test form, they can contain in-built Laravel rules or our custom rules
 	 */
 	protected $rules = array(
-		'name' => array( 'required', 'min:3', 'unique:users' ),
-		// 'email' => array( 'required', 'email', 'unique:users,email'),
 		'role_id' => array('required', 'numeric', 'exists:roles,id')
 	);
+
+	/**
+	 * Add different set of rules depending on if we know the user's id or we are creating a new one
+	 *
+	 * @param string $id 
+	 * @return Void
+	 */
+	public function setRules($id = null)
+	{
+		$array = [];
+
+		if(isset($id))
+		{
+			$array['name']  = array( 'required', 'min:3', 'unique:users,name,'.$id );
+			$array['email'] = array( 'required', 'email', 'unique:users,email,'.$id );
+		} 
+		else 
+		{
+			$array['name']  = array( 'required', 'min:3', 'unique:users,name' );
+			$array['email'] = array( 'required', 'email', 'unique:users,email' );
+		}
+
+		self::addRules($array);
+	}
 } 
