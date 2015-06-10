422ac27f448c715d299490f9c8e696a66770629a
dario-c <dariocavanillas@gmail.com>
2015-06-10 16:07:59 +0200

    Added setRules function which adds different validation rules depending on Post existing or not

--- app/Services/Validation/PostValidator.php
+++ app/Services/Validation/PostValidator.php
@@ -8,8 +8,30 @@ class PostValidator extends Validator {
 	 * @var array Validation rules for the test form, they can contain in-built Laravel rules or our custom rules
 	 */
 	protected $rules = array(
-		'title' => array( 'required' ),
-		'slug'	=> array( 'required' ),
 		'state'	=> array( 'numeric' )
 	);
+
+	/**
+	 * Add different set of rules depending on if we know the Post's id or we are creating a new one
+	 *
+	 * @param string $id 
+	 * @return Void
+	 */
+	public function setRules($id = null)
+	{
+		$array = [];
+
+		if(isset($id))
+		{
+			$array['title'] = array( 'required', 'unique:posts,title,'.$id );
+			$array['slug'] 	= array( 'required', 'unique:posts,slug,'.$id );
+		} 
+		else 
+		{
+			$array['title'] = array( 'required', 'unique:posts,title' );
+			$array['slug'] 	= array( 'required', 'unique:posts,slug' );
+		}
+
+		self::addRules($array);
+	}
 } 
