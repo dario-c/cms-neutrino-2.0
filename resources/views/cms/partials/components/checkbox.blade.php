ba07e07ebd53171db1c9e938213a6372e129f81d
dario-c <dariocavanillas@gmail.com>
2015-06-12 11:25:21 +0200

    Improved checkboxes Id naming

--- resources/views/cms/partials/components/checkbox.blade.php
+++ resources/views/cms/partials/components/checkbox.blade.php
@@ -7,14 +7,15 @@
 				$fill = false;
 				if(isset($post)) $fill = (strpos($post->getMeta($postTypeField->id), $value) !== false) ? true : false;
 			?>
-				{!! Form::checkbox($postTypeField->id.'[]', $index, $fill,
-					[
-						'id'						=> $index,
-						'required'					=> ($postTypeField->parameter('required') == true) ? 'required' : null,
-						'data-fv-notempty'			=> ($postTypeField->parameter('required') == true) ? 'true' : null,
-					    'data-fv-notempty-message' 	=> ($postTypeField->parameter('required') == true) ? 'Please select an option' : null,
-				]) !!}
-				{!! Form::label($index, $index, ['class' => 'selection-label']) !!}
+			
+			{!! Form::checkbox($postTypeField->id.'[]', $index, $fill,
+				[
+					'id'						=> $postTypeField->id.'_'.$index,
+					'required'					=> ($postTypeField->parameter('required') == true) ? 'required' : null,
+					'data-fv-notempty'			=> ($postTypeField->parameter('required') == true) ? 'true' : null,
+				    'data-fv-notempty-message' 	=> ($postTypeField->parameter('required') == true) ? 'Please select an option' : null,
+			]) !!}
+			{!! Form::label($postTypeField->id.'_'.$index, $index, ['class' => 'selection-label']) !!}
 		@endforeach
 	</div>
 </div>
