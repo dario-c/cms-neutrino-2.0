c9f1fcb8a7a69e2aad3560b5f9245c22e72ed589
dario-c <dariocavanillas@gmail.com>
2015-06-16 16:58:49 +0200

    Created a Select Field in RelationComponent's template

--- resources/views/cms/partials/components/post_relation.blade.php
+++ resources/views/cms/partials/components/post_relation.blade.php
@@ -1,3 +1,9 @@
 <div class="form-group">
-	post relation component
+	{!! Form::label($postTypeField->id, $postTypeField->title.(($postTypeField->parameter('required') == true) ? ' *' : '' )) !!}
+	{!! Form::select($postTypeField->id, $postTypeField->source(), (isset($post)) ? $post->getMeta($postTypeField->id) : null,[
+		'class' 					=> 'form-control',
+		'required'					=> ($postTypeField->parameter('required') == true) ? 'required' : null, 
+		'data-fv-notempty'			=> ($postTypeField->parameter('required') == true) ? 'true' : null,
+		'data-fv-notempty-message' 	=> ($postTypeField->parameter('required') == true) ? 'Please select an option' : null,
+	]) !!}
 </div>
