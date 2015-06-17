476490a404b4f3f5902a6a4db56d948876128251
dario-c <dariocavanillas@gmail.com>
2015-06-17 16:48:28 +0200

    RelationComponent's template returns checkboxes or selectbox depending on multiple/single relation

--- resources/views/cms/partials/components/post_relation.blade.php
+++ resources/views/cms/partials/components/post_relation.blade.php
@@ -1,9 +1,31 @@
 <div class="form-group">
-	{!! Form::label($postTypeField->id, $postTypeField->title.(($postTypeField->parameter('required') == true) ? ' *' : '' )) !!}
-	{!! Form::select($postTypeField->id, $postTypeField->source(), (isset($post)) ? $post->getMeta($postTypeField->id) : null,[
-		'class' 					=> 'form-control',
-		'required'					=> ($postTypeField->parameter('required') == true) ? 'required' : null, 
-		'data-fv-notempty'			=> ($postTypeField->parameter('required') == true) ? 'true' : null,
-		'data-fv-notempty-message' 	=> ($postTypeField->parameter('required') == true) ? 'Please select an option' : null,
-	]) !!}
+	
+	@if($postTypeField->parameters['multiple'])
+		<span class="form-label">{{ $postTypeField->title.(($postTypeField->parameter('required') == true) ? ' *' : '')}}</span>
+
+		<div class="selection-options">
+			@foreach($postTypeField->source() as $index => $value)
+				<?php $fill=false ?>
+
+				{!! Form::checkbox($postTypeField->id.'[]', $index, $fill,
+					[
+						'id'						=> $postTypeField->id.'_'.$index,
+						'required'					=> ($postTypeField->parameter('required') == true) ? 'required' : null,
+						'data-fv-notempty'			=> ($postTypeField->parameter('required') == true) ? 'true' : null,
+					    'data-fv-notempty-message' 	=> ($postTypeField->parameter('required') == true) ? 'Please select an option' : null,
+				]) !!}
+				{!! Form::label($postTypeField->id.'_'.$index, $value, ['class' => 'selection-label']) !!}
+			@endforeach
+		</div>
+
+	@else
+		{!! Form::label($postTypeField->id, $postTypeField->title.(($postTypeField->parameter('required') == true) ? ' *' : '' )) !!}
+		{!! Form::select($postTypeField->id, $postTypeField->source(), (isset($post)) ? $post->getMeta($postTypeField->id) : null,[
+			'class' 					=> 'form-control',
+			'required'					=> ($postTypeField->parameter('required') == true) ? 'required' : null, 
+			'data-fv-notempty'			=> ($postTypeField->parameter('required') == true) ? 'true' : null,
+			'data-fv-notempty-message' 	=> ($postTypeField->parameter('required') == true) ? 'Please select an option' : null,
+		]) !!}
+	@endif
+
 </div>
